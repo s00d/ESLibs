1c8b1d2aa841fc4131396488dd088c7cbd89b213
Serafim <nesk@xakep.ru>
2016-02-11 20:56:36 +0300

    Improve event dispatcher

--- Events/Dispatcher.js
+++ Events/Dispatcher.js
@@ -1,4 +1,59 @@
+import Str from "/Support/Str";
 import EventObject from "/Events/EventObject";
+
+/**
+ * Fired event
+ */
+class FiredEvent {
+    /**
+     * @type {string}
+     * @private
+     */
+    _name = '';
+
+    /**
+     * @type {string}
+     * @private
+     */
+    _originalName = '';
+
+    /**
+     * @type {Array}
+     * @private
+     */
+    _args = [];
+
+    /**
+     * @param {string} name
+     * @param {Array} args
+     */
+    constructor(name, ...args) {
+        this._originalName = name;
+        this._name = Str.studlyCase(name);
+        this._args = args;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() : string {
+        return this._name;
+    }
+
+    /**
+     * @returns {*}
+     */
+    get args() : Array {
+        return this._args;
+    }
+
+    /**
+     * @returns {string}
+     */
+    [Symbol.toPrimitive] () {
+        return 'Event ' + this.name;
+    }
+}
 
 /**
  * Event Dispatcher
